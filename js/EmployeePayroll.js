2c26e905a2cd3d4629f42c34379d6cfc506276a1
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-08-26 22:07:21 +0530

    [Shantanu] Added : javaScript file and added Getters and Setters method

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -33,4 +33,22 @@ class EmployeePayrollData {
    set salary(salary) {
       this._salary = salary;
    }
+
+   get note() { return this._note; }
+   set note(note) {
+      this._note = note;
+   }
+
+   get startDate() { return this._startDate; }
+   set startDate(startDate) {
+      this._startDate = startDate;
+   }
+
+   toString() {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' };
+      const empDate = !this.startDate ? "undefined" : this.startDate.toLocalDateString("en-US", options);
+      return "id=" + this.id + ", name=" + this.name + ", gender=" + this.gender +
+         ", profilePic=" + this.profilePic + ", department=" + this.department +
+         ", salary=" + this.salary + ", startDate=" + empDate + ", note=" + this.note;
+   }
 }
