a255a3e62b3aa1574998c7997cd7df02ce1fcd45
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-08-26 22:32:47 +0530

    [Shantanu] Added : On Document Load Set Event Listeners

--- js/new_payroll_form.js
+++ js/new_payroll_form.js
@@ -16,6 +16,31 @@ const getEmployeePayrollDataFromStorage = () => {
         JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
 }
 
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new getEmployeePayrollDataFromStorage()).name = name.value;
+            textError.textContent = "";
+        }
+        catch (e) {
+            textError.textContent = e;
+        }
+    })
+
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function () {
+        output.textContent = salary.value;
+    });
+});
+
 const createInnerHtml = () => {
     var innerHTML1 = "";
     const headerHtml = "<tr>" +
